323058e0f6f47494efb8ef961f152e8827b4df56
chris.sweeney3@gmail.com <chris.sweeney3@gmail.com>
2014-06-06 09:38:53 -0400

    update

--- plot4.R
+++ plot4.R
@@ -9,21 +9,21 @@ png("plot4.png")
 
 #upper left plot
 par(mfrow=c(2,2))
-plot(PowerDS$DateTime, PowerDS$Global_active_power, type="l",xlab="", ylab="Global Active Power (kilowatts)")
+plot(PowerDS$DateTime, PowerDS$Global_active_power, type="l",xlab="", ylab="Global Active Power")
 
 #upper right plot
 plot(PowerDS$DateTime, PowerDS$Voltage, type="l",xlab="", ylab="Voltage")
 title(sub="datetime")
 
 #lower left plot
-plot(PowerDS$DateTime, PowerDS$Sub_metering_1,col="black", type="l", ylab="Sub Metering",xlab="")  
+plot(PowerDS$DateTime, PowerDS$Sub_metering_1,col="black", type="l", ylab="Energy sub metering",xlab="")  
 box()
 lines(PowerDS$DateTime,PowerDS$Sub_metering_2, type="l",col="red")
 lines(PowerDS$DateTime,PowerDS$Sub_metering_3, type="l",col="blue")
 legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty=c(1,1,1),col=c("black","red","blue"))
 
 #lower right plot
-plot(PowerDS$DateTime, PowerDS$Global_reactive_power, type="l",xlab="", ylab="Global reactive power")
+plot(PowerDS$DateTime, PowerDS$Global_reactive_power, type="l",xlab="", ylab="Global_reactive_power")
 title(sub="datetime")
 
 dev.off()
